76728b8d1b6973b059a066d9c49af172fec9b622
APedrosa13 <45878777+AntonioMSPMoreira13@users.noreply.github.com>
2024-04-29 16:31:11 +0100

    drawer is working

--- APP/app/src/main/java/com/pathfinder_helper/app/MainActivity.kt
+++ APP/app/src/main/java/com/pathfinder_helper/app/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.pathfinder_helper.app
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import com.pathfinder_helper.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +22,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         enableEdgeToEdge()
 
+        // Inicialização das visualizações
+        drawerLayout = findViewById(R.id.main)
+        navView = findViewById(R.id.nav_view)
+        toolbar = findViewById(R.id.toolbar)
+        // Configuração da Toolbar
+        setSupportActionBar(toolbar)
+
+        // Configuração do ActionBarDrawerToggle
+        navView.bringToFront()
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Navbar
         replaceFragment(Lore())
 
         binding.navbar.setOnItemSelectedListener {
